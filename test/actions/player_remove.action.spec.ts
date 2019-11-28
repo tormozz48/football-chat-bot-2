20c3790dedf328ffc756be851393346b24f44ffb
Andre <andrey.kuznetsov48@yandex.ru>
2019-11-28 15:13:34 +0300

    ci(tests): collect code covarage
    Add tests for /remove command. Add codecov CI integration

--- test/actions/player_remove.action.spec.ts
+++ test/actions/player_remove.action.spec.ts
@@ -6,6 +6,7 @@ import {AppEmitter} from '../../src/common/event-bus.service';
 import {StorageService} from '../../src/storage/storage.service';
 import * as statuses from '../../src/actions/statuses';
 import {Chat} from '../../src/storage/models/chat';
+import {Player} from '../../src/storage/models/player';
 import {IParams} from '../../src/common/template.service';
 
 describe('PlayerRemoveAction', () => {
@@ -59,6 +60,99 @@ describe('PlayerRemoveAction', () => {
                         resolve,
                     );
                     appEmitter.emit(appEmitter.EVENT_ADD, ctx);
+                });
+            });
+
+            it('should return no_player response if player was not found by given name', async () => {
+                const jsonRes: string = await new Promise(resolve => {
+                    const ctx = createContextStub({text: 'John Smith'}, resolve);
+                    appEmitter.emit(appEmitter.PERSON_REMOVE, ctx);
+                });
+
+                const {params}: {params: IParams} = JSON.parse(jsonRes);
+                expect(params.status).toBe(statuses.STATUS_NO_PLAYER);
+            });
+
+            describe('player exist', () => {
+                async function assertExistedPlayer_() {
+                    const players: Player[] = await storageService.connection
+                        .getRepository(Player)
+                        .find();
+                    expect(players[0].name).toBe('John Smith');
+                }
+
+                async function assertNotExistedPlayer_() {
+                    const playersCount: number = await storageService.connection
+                        .getRepository(Player)
+                        .count();
+
+                    expect(playersCount).toBe(0);
+                }
+
+                beforeEach(async () => {
+                    await new Promise(resolve => {
+                        const ctx = createContextStub({text: 'John Smith'}, resolve);
+                        appEmitter.emit(appEmitter.PERSON_ADD, ctx);
+                    });
+                });
+
+                it('should remove player with given name', async () => {
+                    await assertExistedPlayer_();
+
+                    await new Promise(resolve => {
+                        const ctx = createContextStub({text: 'John Smith'}, resolve);
+                        appEmitter.emit(appEmitter.PERSON_REMOVE, ctx);
+                    });
+
+                    await assertNotExistedPlayer_();
+                });
+
+                it('should remove message owner as player if name was not set', async () => {
+                    await assertExistedPlayer_();
+
+                    await new Promise(resolve => {
+                        const ctx = createContextStub({firstName: 'John', lastName: 'Smith'}, resolve);
+                        appEmitter.emit(appEmitter.PERSON_REMOVE, ctx);
+                    });
+
+                    await assertNotExistedPlayer_();
+                });
+
+                it('should return success result', async () => {
+                    const jsonRes: string = await new Promise(resolve => {
+                        const ctx = createContextStub({text: 'John Smith'}, resolve);
+                        appEmitter.emit(appEmitter.PERSON_REMOVE, ctx);
+                    });
+
+                    const {params}: {params: IParams} = JSON.parse(jsonRes);
+                    expect(params.status).toBe(statuses.STATUS_SUCCESS);
+                });
+
+                it('should inlude name of removed player into result', async () => {
+                    const jsonRes: string = await new Promise(resolve => {
+                        const ctx = createContextStub({text: 'John Smith'}, resolve);
+                        appEmitter.emit(appEmitter.PERSON_REMOVE, ctx);
+                    });
+
+                    const {data} = JSON.parse(jsonRes);
+                    expect(data.name).toBe('John Smith');
+                });
+
+                it('should include list of players into result', async () => {
+                    await new Promise(resolve => {
+                        const ctx = createContextStub({text: 'Jack Wayne'}, resolve);
+                        appEmitter.emit(appEmitter.PERSON_ADD, ctx);
+                    });
+
+                    const jsonRes: string = await new Promise(resolve => {
+                        const ctx = createContextStub({text: 'John Smith'}, resolve);
+                        appEmitter.emit(appEmitter.PERSON_REMOVE, ctx);
+                    });
+
+                    const {data} = JSON.parse(jsonRes);
+                    expect(data.total).toBe(1);
+                    expect(data.players[0].index).toBe(1);
+                    expect(data.players[0].name).toBe('Jack Wayne');
                 });
             });
         });
